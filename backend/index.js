b34a00e4bf0e2deed9ac0964ede0c4f4b64e5d04
Sparsh Singh <sparshsingh03012003@gmail.com>
2025-06-06 20:47:18 +0530

    FIrst commit

---
+++ backend/index.js
@@ -0,0 +1,49 @@
+// Index.js
+
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/database");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+
+dotenv.config();
+
+const app = express();
+
+// MIddlewares
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+// Rate limiting
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
+app.use(limiter);
+
+const authRouter = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+
+// Mount routes
+app.use("/api/auth", authRouter);
+app.use("/api/products", productRoutes);
+
+//<Checking is API live>  
+app.get("/", (req, res) => {
+  res.send("API is running…");
+});
+
+// Error handler (basic)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Server Error", error: err.message });
+});
+
+// Connect to MongoDB
+connectDB();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,24 @@
+// ecommerce-backend/routes/productRoutes.js
+
+const express = require("express");
+const router = express.Router();
+const {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productController");
+
+const { protect, authorizeRole } = require("../middlewares/authMiddleware");
+
+// Public routes
+router.get("/", getAllProducts);
+router.get("/:id", getProductById);
+
+// Private routes (seller only)
+router.post("/", protect, authorizeRole("seller"), createProduct);
+router.put("/:id", protect, authorizeRole("seller"), updateProduct);
+router.delete("/:id", protect, authorizeRole("seller"), deleteProduct);
+
+module.exports = router;
